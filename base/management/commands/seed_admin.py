bfa50ec92f99f35a5332b0e30b6ea4bfabcde99a
Hong Nhung <hongnhung2001thd@gmail.com>
2026-03-07 05:01:40 +0900

    Deploy

--- base/management/commands/seed_admin.py
+++ base/management/commands/seed_admin.py
@@ -1,10 +1,22 @@
-from django.http import HttpResponse
+from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
-def reset_my_admin(request):
-    user, created = User.objects.get_or_create(username='nhung')
-    user.set_password('887675nh') 
-    user.is_staff = True
-    user.is_superuser = True
-    user.save()
-    return HttpResponse("Xong! User: nhung | Pass: 887675nh")
+class Command(BaseCommand):
+    help = 'Tu dong tao tai khoan Admin'
+
+    def handle(self, *args, **options):
+        username = 'nhung'
+        email = 'admin@gmail.com'
+        password = '887675nh'
+        
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username, email, password)
+            self.stdout.write(self.style.SUCCESS(f'Da tao Admin thanh cong: {username}'))
+        else:
+
+            user = User.objects.get(username=username)
+            user.set_password(password)
+            user.is_staff = True
+            user.is_superuser = True
+            user.save()
+            self.stdout.write(self.style.SUCCESS(f'Da cap nhat mat khau cho Admin: {username}'))
