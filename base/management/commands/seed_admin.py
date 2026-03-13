fe0e17eb889aa660aec8c73b50df894a14457d8f
Hong Nhung <hongnhung2001thd@gmail.com>
2026-03-13 14:41:28 +0900

    Deploy

--- base/management/commands/seed_admin.py
+++ base/management/commands/seed_admin.py
@@ -4,24 +4,24 @@ from base.models import Product
 from base.products import products
 
 class Command(BaseCommand):
-    help = 'Tu dong tao tai khoan Admin'
+    # help = 'Tu dong tao tai khoan Admin'
 
     def handle(self, *args, **options):
-        username = 'ad'
-        email = 'ad6@gmail.com'
-        password = '887675nh'
+        # username = 'ad'
+        # email = 'ad6@gmail.com'
+        # password = '887675nh'
         
-        if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(username, email, password)
-            self.stdout.write(self.style.SUCCESS(f'Da tao Admin thanh cong: {username}'))
-        else:
+        # if not User.objects.filter(username=username).exists():
+        #     User.objects.create_superuser(username, email, password)
+        #     self.stdout.write(self.style.SUCCESS(f'Da tao Admin thanh cong: {username}'))
+        # else:
 
-            user = User.objects.get(username=username)
-            user.set_password(password)
-            user.is_staff = True
-            user.is_superuser = True
-            user.save()
-            self.stdout.write(self.style.SUCCESS(f'Da cap nhat mat khau cho Admin: {username}'))
+        #     user = User.objects.get(username=username)
+        #     user.set_password(password)
+        #     user.is_staff = True
+        #     user.is_superuser = True
+        #     user.save()
+        #     self.stdout.write(self.style.SUCCESS(f'Da cap nhat mat khau cho Admin: {username}'))
         
         
 
